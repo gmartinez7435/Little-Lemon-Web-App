915d514248878e5971c5595876a519ab0059e4a5
gmartinez7435 <gmartinez7435@gmail.com>
2024-11-26 22:36:33 -0700

    new components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
+import React from'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Homepage
+    <>
+    <Header />
+    <Nav />
+    <Main />
+    <Footer />
+
+     
       
-    </div>
+    </>
   );
 }
 
---
+++ my-app/src/components/Nav.js
@@ -0,0 +1,32 @@
+import React from "react";
+import lllogo from "../assets/lllogo.jpg";
+export default function Nav() {
+  return (
+    <>
+      <img src={lllogo} alt="Logo" className="nav-logo" />
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="/order-online">Order Online</a>
+          </li>
+          <li>
+            <a href="/login">Log In</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
