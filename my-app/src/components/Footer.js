ba35daaff04e7fd70d46f56f9e70dda4f4c922d2
gmartinez7435 <gmartinez7435@gmail.com>
2024-11-26 22:46:34 -0700

    nav to Footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import lllogo from "../assets/lllogo.jpg";
 
 export default function Footer() {
+
   return (
-    <div>Footer</div>
+    <>
+      <img src={lllogo} alt="Logo" className="nav-logo" />
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="/order-online">Order Online</a>
+          </li>
+          <li>
+            <a href="/login">Log In</a>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
