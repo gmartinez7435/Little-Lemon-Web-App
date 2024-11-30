cf0d01f5148191671896cd9d146facb8dca79f6f
gmartinez7435 <gmartinez7435@gmail.com>
2024-11-29 17:09:58 -0700

    changes

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,56 +1,54 @@
-import React from 'react'
-import lllogo from "../assets/lllogo.jpg";
+import React from "react";
+import footerLogo from "../assets/footer_Logo.png";
 
 export default function Footer() {
-
   return (
     <footer className="nav-footer">
-      <img src={lllogo} alt="Logo" className="ftnav-logo" />
-      <nav className="footer-nav">
-        
-        <ul>
-          <label>Doormat<br>
-          </br> Navigation</label>
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/menu">Menu</a>
-          </li>
-          <li>
-            <a href="/reservations">Reservations</a>
-          </li>
-          <li>
-            <a href="/order-online">Order Online</a>
-          </li>
-          <li>
-            <a href="/login">Log In</a>
-          </li>
-        </ul>
-      </nav>
+      <img src={footerLogo} alt="Logo" className="ftnav-logo" />
+        <nav className="footer-nav">
+          <ul>
+            <label>
+              Doormat<br></br> Navigation
+            </label>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+              <a href="/reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="/order-online">Order Online</a>
+            </li>
+            <li>
+              <a href="/login">Log In</a>
+            </li>
+          </ul>
+        </nav>
 
-    <nav className='contact-nav'>
-      
-      <ul>
-        <label>Contact</label>
-        <li>Address</li>
-        <li>Phone Number</li>
-        <li>Email</li>
-      </ul>
-    </nav>
+        <nav className="contact-nav">
+          <ul>
+            <label>Contact</label>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>Email</li>
+          </ul>
+        </nav>
 
-    <nav className='social-media-nav'>
-      
-      <ul>
-        <label>Social Media Links</label>
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>Twitter</li>
-      </ul>
-    </nav>
+        <nav className="social-media-nav">
+          <ul>
+            <label>Social Media Links</label>
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+          </ul>
+        </nav>
     </footer>
-  )
+  );
 }
+
