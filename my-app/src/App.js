6960eb3b28bc370ae394a6ffc88bb5d12d6db504
gmartinez7435 <gmartinez7435@gmail.com>
2024-12-08 20:37:38 -0700

    Revert "made the footer"
    This reverts commit 622ad080483d433cbd61d9a557f7213b721d197e.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Menu from './components/Menu';
-import About from './components/About';
-import Footer from './components/Footer';
-import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/about" element={<About />} />
-        {/* Add other routes as needed */}
-      </Routes>
-      <Footer />
+      <Main />
+      <Menu />
     </>
   );
 }
